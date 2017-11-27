1cfda648572b85c4533e9e77a92b3b61e73fe8dd
hudson <archreth@gmail.com>
2017-11-27 11:28:06 -0800

    working sly

--- public/js/p9.js
+++ public/js/p9.js
@@ -1,4 +1,37 @@
+
+var slyelement = {
+      obj: {},
+        el: '.frame',
+          options: {
+                  horizontal: 1,
+                      itemNav: 'forceCentered',
+                          smart: 1,
+                              activateMiddle: 1,
+                                  activateOn: 'click',
+                                      mouseDragging: 1,
+                                          touchDragging: 1,
+                                              releaseSwing: 1,
+                                                  startAt: 0,
+                                                      scrollBy: 1,
+                                                          speed: 300,
+                                                              elasticBounds: 1,
+                                                                  easing: 'swing', // easeInOutElastic, easeOutBounce
+                                                                      scrollBar: $('.scrollbar')
+                                                                        }
+};
+
+
+$(function(){
+      slyelement.obj = new Sly($(slyelement.el), slyelement.options);
+        
+          slyelement.obj.init();
+});
+
+$(window).resize(function(e) {
+      slyelement.obj.reload();
+});
 document.onkeydown= function (event) {
+
 
     if (event.which == 37 || event.keyCode == 37) {
             console.log("left"); 
@@ -16,5 +49,4 @@ document.onkeydown= function (event) {
     return true;
 
 };
-
 
